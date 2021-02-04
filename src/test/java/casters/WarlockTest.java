f0f6dac7acf7c60d301076090c5e4c6c23189007
Laura Dobie <moving.toyshop@gmail.com>
2021-02-04 16:01:07 +0000

    Warlock tests pass

--- src/test/java/casters/WarlockTest.java
+++ src/test/java/casters/WarlockTest.java
@@ -1,4 +1,54 @@
 package casters;
 
+import enemies.Orc;
+import goodpets.Ogre;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WarlockTest {
+    Fireball fireball;
+    Lightning lightning;
+    Warlock warlock;
+    Orc orc;
+    Ogre ogre;
+
+    @Before
+    public void before(){
+        fireball = new Fireball();
+        lightning = new Lightning();
+        ogre = new Ogre();
+        orc = new Orc(120);
+        warlock = new Warlock("James", 100, lightning, ogre);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("James", warlock.getName());
+    }
+
+    @Test
+    public void hasHealth(){
+        assertEquals(100, warlock.getHealth());
+    }
+
+    @Test
+    public void canCastSpell(){
+        warlock.cast(orc);
+        assertEquals(100, warlock.getHealth());
+    }
+
+    @Test
+    public void canChangeSpell(){
+        warlock.setSpell(fireball);
+        warlock.cast(orc);
+        assertEquals(100, orc.getHealth());
+    }
+
+    @Test
+    public void canDefend(){
+        warlock.defend(orc);
+        assertEquals(70, orc.getHealth());
+    }
 }
